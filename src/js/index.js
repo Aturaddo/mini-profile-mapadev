cbb199b118d2109657c9c1bf8a14fc6cc156b876
Arthur Negri <aturaddo@gmail.com>
2022-10-23 14:54:01 -0300

    finish JS, CLick Tabs and change infos

--- src/js/index.js
+++ src/js/index.js
@@ -7,9 +7,25 @@ tabs.forEach(tab => {
       return
     }
 
-    const tabSelect = document.querySelector('.tab.select')
-    tabSelect.classList.remove('select') // Remover uma Classe no HTML
-
-    tab.classList.add('select') // Adiciona uma Classe no HTML
+    selectTab(tab) //function selecao Abas
+    infoSelectTab(tab)
   })
 })
+
+function selectTab(tab) {
+  const tabSelect = document.querySelector('.tab.select')
+  tabSelect.classList.remove('select') // Remover uma Classe no HTML
+
+  tab.classList.add('select') // Adiciona uma Classe no HTML
+}
+
+function infoSelectTab(tab) {
+  const infoSelect = document.querySelector('.info.select')
+  infoSelect.classList.remove('select')
+
+  const idElementInfoTabs = `info-${tab.id}` // Buscar ID em HTML
+  console.log(idElementInfoTabs)
+
+  const infoScreen = document.getElementById(idElementInfoTabs)
+  infoScreen.classList.add('select')
+}
